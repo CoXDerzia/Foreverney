4da3250106d09f834c453cbb1f57a4a309128fc2
CoXDerzia <rty82045@gmail.com>
2026-04-14 15:59:16 +0300

    Update ПЗ6-3.py

--- ПЗ-6/ПЗ6-3.py
+++ ПЗ-6/ПЗ6-3.py
@@ -3,5 +3,10 @@ x1 = random.randint(1,8)
 y1 = random.randint(1,8)
 x2 = random.randint(1,8)
 y2 = random.randint(1,8)
-1kletka = (x1+y1) % 2 =0
-print(x1,y1,x2,y2)
+kletka1 = (x1+y1)%2
+kletka2 = (x2+y2)%2
+if kletka1 == kletka2:
+    print("YES")
+else:
+    print("NO")
+print(kletka1, kletka2))
