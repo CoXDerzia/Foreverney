42dedc2900e90a75205e07dce2e064b0bdf65ba3
CoXDerzia <rty82045@gmail.com>
2026-04-14 15:10:51 +0300

    Add files via upload

---
+++ ПЗ-6/ПЗ6-1.py
@@ -0,0 +1,5 @@
+temp = int(input("Введите температуру: "))
+pressure = int(input("Введите давление: "))
+pulse = int(input("Введите пульс: "))
+if temp >=36 and temp <=37 and pressure >=110 and pressure <=130 and pulse >=60 and pulse <=100:
+    print("Нормальное состояние")
