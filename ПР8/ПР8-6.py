32cecd8f87eb37021dd9712ef5577bf3173edc39
CoXDerzia <rty82045@gmail.com>
2026-05-08 15:16:09 +0300

    Update ПР8-6.py

--- ПР8/ПР8-6.py
+++ ПР8/ПР8-6.py
@@ -1,23 +1,17 @@
-print("Программа зашадывает случайное число и ваша задача угадать правильное число")
+print("Программа загадывает случайное число и ваша задача угадать правильное число")
 
 import random
 
 passw = random.randint(1, 10)
-attempts = 0
-max_attempts = 3
 
-while attempts < max_attempts:
+for attempt in range(1, 4):
     guess = int(input("Напишите число от 1 до 10:"))
-    attempts += 1
-
     if guess == passw:
         print("Вы угадали правильное число!")
         break
-    elif attempts == max_attempts:
-        print(f"Вы потратили все попытки. Верное число было {passw}.")
-        break
+    elif guess > passw:
+        print(f"Неверно. Загаднное число меньше вашего.")
     else:
-        if guess > passw:
-            print(f"Неверно. Загаднное число меньше вашего. Это была {attempts} попытка.")
-        else:
-            print(f"Неверно. Загаднное число больше вашего. Это была {attempts} попытка.")
+        print(f"Неверно. Загаднное число больше вашего.")
+else:
+    print(f"Вы потратили все попытки. Верное число было {passw}.")
